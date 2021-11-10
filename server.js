ad66c60e9316d26955f6c232a8f565f33c336d21
Tibaran <palacios.tibaran@gmail.com>
2021-11-10 18:58:50 -0500

    arreglo en nombre

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/peliculas-app'));
+app.use(express.static(__dirname+'/dist/peliculasApp'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/peliculas-app/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/peliculasApp/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
